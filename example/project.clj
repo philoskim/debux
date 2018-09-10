fb7143287305b9829c0b7fb387bf3b8cc26a9991
philoskim <philos99@gmail.com>
2018-09-10 18:15:52 +0900

    example/project.clj modified

--- example/project.clj
+++ example/project.clj
@@ -1,6 +1,6 @@
 (defproject example "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.854"]
+                 [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/core.async "0.3.465"]
                  [philoskim/debux "0.5.1"]]
   :plugins [[lein-cljsbuild "1.1.6"]
