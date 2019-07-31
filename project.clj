789f43d16bd1d00b0a20b561a0473e887abfea81
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-07-31 12:37:14 +0100

    add :scope provided to clojure and clojurescript, to make sure they are not brought in as dependencies (specially clojurescript) in cases where they are not used by the project being debugged.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/philoskim/debux"
   :license {"Eclipse Public License"
             "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.10.238"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.238" :scope "provided"]
                  [clojure-future-spec "1.9.0-alpha17"]]
   :source-paths ["src"])
