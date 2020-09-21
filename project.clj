2de50339c43004f0ca555cf66fb2a2bfbf6d32ed
Philos Kim <philos99@gmail.com>
2020-09-21 12:12:58 +0900

    v0.7.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject philoskim/debux "0.7.5"
+(defproject philoskim/debux "0.7.6"
   :description "A trace-based debugging library for Clojure and ClojureScript"
   :url "https://github.com/philoskim/debux"
   :license {:name "Eclipse Public License - v 1.0"
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [clojure-future-spec "1.9.0"]
-                 [binaryage/devtools "1.0.0"]]
+                 [binaryage/devtools "1.0.2"]]
   :source-paths ["src"])
