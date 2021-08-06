6c45551b0b97c2e58b8bd1d1953b86dd3aa90f1c
Philos Kim <philos99@gmail.com>
2021-08-06 19:50:27 +0900

    doc modified

--- examples/src/clj/examples/lab.clj
+++ examples/src/clj/examples/lab.clj
@@ -2,17 +2,26 @@
 
 (use 'debux.core)
 
-(defn my-add [a b]
-  (dbg (+ a b) :level 3))
-
+;; The default debug level is 0.
+(dbg (+ 10 20))
+(dbg (+ 10 20 3) :level 3)
+(dbg (+ 10 20 5) :level 5)
 
 (with-level 3
-  (my-add 1 1))
+  (dbg (+ 10 20))
+  (dbg (+ 10 20 3) :level 3)
+  (dbg (+ 10 20 5) :level 5))
 
-(with-level 5
-  (my-add 2 2))
 
-(with-level 5
-  (dbg (my-add 3 3) :level 5))
+(defn my-add [a b]
+  (dbg (+ a b) :level 2))
 
-(dbg (+ 4 4))
+(defn my-sub [a b]
+  (dbg (- a b) :level 3))
+
+(with-level 3
+  (dbg (my-add 10 20))
+  (dbg (my-sub 100 10))
+
+  (with-level 0
+    (dbg (* 10 2))))
