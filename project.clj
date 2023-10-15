de0b5fbdc59684505928b76e3db5815ac5a913c7
George Lipov <gnl@lipov.net>
2023-10-15 18:25:07 +0200

    Upgrade Clojure to 1.10.0 for `requiring-resolve`

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject philoskim/debux "0.8.3"
+(defproject com.github.gnl/debux "0.8.4-1"
   :description "A trace-based debugging library for Clojure and ClojureScript"
   :url "https://github.com/philoskim/debux"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.238" :scope "provided"]]
   :source-paths ["src"])
