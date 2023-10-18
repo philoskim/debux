d68381bd17fe6259f45a6111de9c4160fd31faa4
Philos Kim <philos99@gmail.com>
2023-10-18 19:19:23 +0900

    test:bb code tweaked

--- test/debux/core_test.clj
+++ test/debux/core_test.clj
@@ -1,9 +1,10 @@
 (ns debux.core-test
   (:require [clojure.test :refer [deftest is testing] :as t]
-            [debux.core]
-            [debux.cs.core]
             [clojure.string :as str]))
+
+(use 'debux.core)
 
 (deftest dbg-test
   (testing "dbg reader conditional works"
     (is (str/includes? (with-out-str #d/dbg [1 2 3]) "dbg: [1 2 3]" ))))
+
